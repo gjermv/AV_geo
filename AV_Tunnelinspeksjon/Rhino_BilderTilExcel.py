bfb6f004d362f0d383cb698731b5f212e03d5303
gjermv <gjermund.vingerhagen@gmail.com>
2024-10-30 19:48:47 +0100

    Excel-ark som kan leses inn
    Oppretter Excel-ark som kan leses inn fra Rhino_RegisteringerFraExcel.py, og senere brukes ifm skaderegistreringsskjema.

--- AV_Tunnelinspeksjon/Rhino_BilderTilExcel.py
+++ AV_Tunnelinspeksjon/Rhino_BilderTilExcel.py
@@ -14,16 +14,39 @@ import glob
 import re
 import System.Windows.Forms as forms
 
+import locale
+locale.setlocale(locale.LC_ALL, 'nb_NO')
+
+import pandas as pd
+
 
 
 def BildemappeTilExcel(bilde_mappe):
-    for image in glob.glob(bilde_mappe):
+    
+    bildeFiler = bilde_mappe + '\\*.[jJ][pP]*[gG]' # Burde matche jpg, JPG og jpeg
+    bildeOversikt = []
+    
+    for image in glob.glob(bildeFiler):
+        d = {'Pel': 'NA',
+             'Pos': 'NA',
+             'Side': 'NA',
+             'Symbol': 'NA',
+             'Bildenavn': 'NA',
+             'Kommentar': '',
+             'Plassforhold': '',
+             'Retning': '',
+             'Tiltak': '',
+             'Plassering': '',
+             'Nedfall': ''}
+        
+        
         try:
             img = PIL.Image.open(image)
             exif_data = img._getexif()
 
             head, tail = os.path.split(image)
-
+            d['Bildenavn'] = tail
+            
             try:
                 image_text = exif_data[270]
 
@@ -37,6 +60,14 @@ def BildemappeTilExcel(bilde_mappe):
                         t = re.sub(r'[^a-zA-Z0-9\s]', '', txt)
 
                         strings = [linje0[0], linje0[1], linje1, t, tail]
+                        
+                        d['Side'] = linje0[0]
+                        d['Pel'] = linje0[1]
+                        d['Symbol'] = linje1
+                        d['Kommentar'] = t
+                        
+                        
+                        
                         line_to_copy = '\t'.join(strings)
 
                         print(line_to_copy)
@@ -49,6 +80,12 @@ def BildemappeTilExcel(bilde_mappe):
 
                         strings = [linje0[0], linje0[1], '-', txt, tail]
                         line_to_copy = '\t'.join(strings)
+                        
+                        
+                        d['Side'] = linje0[0]
+                        d['Pel'] = linje0[1]
+                        d['Symbol'] = '-'
+                        d['Kommentar'] = txt
 
                         print(line_to_copy)
 
@@ -61,21 +98,37 @@ def BildemappeTilExcel(bilde_mappe):
                     strings = [linje0[0], linje0[1], linje1, linje2, tail]
                     line_to_copy = '\t'.join(strings)
                     print(line_to_copy)
-
+                    d['Side'] = linje0[0]
+                    d['Pel'] = linje0[1]
+                    d['Symbol'] = '-'
+                    d['Kommentar'] = txt
+                
                 else:
                     head, tail = os.path.split(image)
+                    d['Symbol'] = 'Feil format'
+                    d['Kommentar'] = image_text                   
+                    
                     print('Error:', image_text)
-
+                    
+                bildeOversikt.append(d)
 
             except:
+                d['Symbol'] = 'Bildefeil - Ingen tekst'
+                d['Kommentar'] = '-'    
                 print('Feil bilde', image)
+                bildeOversikt.append(d)
+        
         except:
-            print('Ikke et bilde', image)
+            d['Symbol'] = 'Ikke bilde'
+            d['Kommentar'] = '-'
+            
+  
+    return bildeOversikt
 
 
 def get_directory():
     dialog = forms.FolderBrowserDialog()
-    dialog.Description = "Velg mappen med bilder"
+    dialog.Description = "Velg mappe med jpg-bilder"
     dialog.ShowNewFolderButton = True
 
     if dialog.ShowDialog() == forms.DialogResult.OK:
@@ -85,10 +138,14 @@ def get_directory():
 
 if __name__ == '__main__':
     bilde_mappe = get_directory()
-    print(bilde_mappe)
+    excelFilePath = os.path.join(bilde_mappe,'BildeOversikt.xlsx')
+
     if os.path.isdir(bilde_mappe):
-        bilde_mappe = f'{bilde_mappe}\\*'
-        print(bilde_mappe)
-        BildemappeTilExcel(bilde_mappe)
+        res = BildemappeTilExcel(bilde_mappe)
+        
+        df = pd.DataFrame.from_dict(res)
+        
+        df.to_excel(excelFilePath)
+        
     else:
         print('Filområdet finnes ikke')
